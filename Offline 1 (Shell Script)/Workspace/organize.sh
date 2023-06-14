8b93a92b6128305899480dbc425302bcbb03ed43
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2023-06-14 06:36:30 +0600

    Unzip submission files

--- Offline 1 (Shell Script)/Workspace/organize.sh
+++ Offline 1 (Shell Script)/Workspace/organize.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "Hello world"
+organize() {
+    # unzip all the folders in $1
+    for i in "$1"/*
+    do
+        if [ -f "$i" ] && [[ $i = *.zip ]]
+        then
+            echo "filename is $i"
+            dirname=${i::-4}
+            echo "dirname is $dirname"
+            mkdir -p "$dirname"
+            unzip "$i" -d "$dirname"
+        fi
+    done
+}
+
+organize "$1"
