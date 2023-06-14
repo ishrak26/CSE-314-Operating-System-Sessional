f9f23bf3c2554e53791ca7ea20e43e95710eb6ce
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2023-06-14 18:58:43 +0600

    Copy and organize C files

--- Offline 1 (Shell Script)/Workspace/organize.sh
+++ Offline 1 (Shell Script)/Workspace/organize.sh
@@ -1,18 +1,81 @@
 #!/bin/bash
 
-organize() {
+unzip_submissions() {
     # unzip all the folders in $1
     for i in "$1"/*
     do
-        if [ -f "$i" ] && [[ $i = *.zip ]]
+        if [ -f "$i" ] && [[ "$i" = *.zip ]]
         then
             echo "filename is $i"
             dirname=${i::-4}
             echo "dirname is $dirname"
             mkdir -p "$dirname"
-            unzip "$i" -d "$dirname"
+            unzip -o "$i" -d "$dirname"
         fi
     done
 }
 
-organize "$1"
+visit()
+{
+	if [ -d "$1" ]
+	then
+	
+		for i in "$1"/*
+		do
+			visit "$i" "$2" "$3"
+		done
+	
+	elif [ -f "$1" ] && { [[ "$1" = *.c ]] || [[ "$1" = *.java ]] || [[ "$1" = *.py ]]; }
+	then
+		# echo "$i"
+        # filename="${i##*/}"
+        # echo "$filename"
+        filename=$(basename "$i")
+        # echo "$filename"
+        extension="${i##*.}"
+        # echo "$extension"
+        # dir=$(dirname "$i")
+        # echo "$dir"
+        
+        if [ "$extension" = "c" ] 
+        then
+            target_dir="$2"
+            target_dir+="/C/"
+            target_dir+="$3"
+            # echo "$target_dir"
+            mkdir -p "$target_dir"
+            cp "$1" "$target_dir"
+
+            if [ "$filename" != "main.c" ] 
+            then
+                target_file="$target_dir"
+                target_file+="/"
+                new_target_file="$target_file"
+                target_file+="$filename"
+                new_target_file+="main.c"
+                mv "$target_file" "$new_target_file"
+            fi
+
+        fi
+
+	fi
+}
+
+organize_files() {
+    mkdir -p "${2}/C"
+    mkdir -p "${2}/Java"
+    mkdir -p "${2}/Python"
+    
+    for i in "$1"/*
+    do
+        if [ -d "$i" ]
+        then
+           sid=${i: -7} # last 7 characters 
+           visit "$i" "$2" "$sid"
+        fi
+    done
+}
+
+unzip_submissions "$1"
+
+organize_files "$1" "$2"
