f34f2df364e37728b44a84f82ca6c149c8a44075
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2023-06-18 00:17:42 +0600

    Implement optional arguments

--- Offline 1 (Shell Script)/Workspace/organize.sh
+++ Offline 1 (Shell Script)/Workspace/organize.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+verbose_flag=false
+noexec_flag=false
+
 unzip_submissions() {
     # unzip all the folders in $1
     for i in "$1"/*
     do
         if [ -f "$i" ] && [[ "$i" = *.zip ]]
         then
-            # echo "filename is $i"
             dirname=${i::-4}
-            # echo "dirname is $dirname"
             mkdir -p "$dirname"
             unzip -oq "$i" -d "$dirname"
         fi
@@ -17,19 +18,14 @@ unzip_submissions() {
 
 relocate_code_file() {
     target_dir="${2}${4}${3}"
-    # target_dir+="$4"
-    # target_dir+="$3"
-    # echo "$target_dir"
     mkdir -p "$target_dir"
     cp "$1" "$target_dir"
 
     if [ "$filename" != "$5" ] 
     then
         target_file="${target_dir}/"
-        # target_file+="/"
         new_target_file="${target_file}${5}"
         target_file+="$filename"
-        # new_target_file+="$5"
         mv "$target_file" "$new_target_file"
     fi
 }
@@ -46,15 +42,10 @@ visit()
 	
 	elif [ -f "$1" ] && { [[ "$1" = *.c ]] || [[ "$1" = *.java ]] || [[ "$1" = *.py ]]; }
 	then
-		# echo "$i"
         # filename="${i##*/}"
-        # echo "$filename"
         filename=$(basename "$i")
-        # echo "$filename"
         extension="${i##*.}"
-        # echo "$extension"
         # dir=$(dirname "$i")
-        # echo "$dir"
         
         if [ "$extension" = "c" ] 
         then
@@ -80,6 +71,10 @@ organize_files() {
         if [ -d "$i" ]
         then
            sid=${i: -7} # last 7 characters 
+           if [ $verbose_flag == "true" ]
+           then
+                echo "Organizing files of ${sid}"
+           fi
            visit "$i" "$2" "$sid"
         fi
     done
@@ -93,7 +88,6 @@ run_tests() {
             num=$(basename "$i")
             num=${num:4}
             cmd="${3} < ${i} > ${1}/out${num}"
-            # echo "$cmd"
             eval " $cmd"
         fi
     done
@@ -107,6 +101,11 @@ execute_C_file() {
             sid=$(basename "$i")
             file_name="${i}/main.c"
             executable_name="${i}/main.out"
+
+            if [ $verbose_flag == "true" ]
+            then
+                echo "Executing files of ${sid}"
+            fi
             
             # compile
             gcc "$file_name" -o "$executable_name"
@@ -126,6 +125,11 @@ execute_Java_file() {
         then
             sid=$(basename "$i")
             file_name="${i}/Main.java"
+
+            if [ $verbose_flag == "true" ]
+            then
+                echo "Executing files of ${sid}"
+            fi
             
             # compile
             javac "$file_name"
@@ -146,6 +150,11 @@ execute_Python_file() {
             sid=$(basename "$i")
             file_name="${i}/main.py"
 
+            if [ $verbose_flag == "true" ]
+            then
+                echo "Executing files of ${sid}"
+            fi
+
             # run the executables
             dir=$(dirname "$file_name")
             cmd="python3 ${file_name}"
@@ -161,7 +170,6 @@ execute_files() {
         then
             
             dir=$(basename "$i")
-            # echo "dir is ${dir}"
             case "$dir" in
                 "C") 
                     execute_C_file "$i" "$2";;
@@ -212,13 +220,49 @@ match_files() {
     done
 }
 
+count_test_files() {
+    cnt=0
+    for i in "$1"/*
+    do
+        if [ -f "$i" ] && [[ "$i" = *.txt ]]
+        then
+            cnt=$((cnt+1))
+        fi
+    done
+    echo "Found ${cnt} test files"
+}
+
+check_usage() {
+    echo ""
+}
+
+# Loop through the command-line arguments
+i=1
+for arg in "$@"; do
+    if [ "$i" -gt 4 ] 
+    then
+        if [ "$arg" = "-v" ] 
+        then
+            verbose_flag=true;
+        elif [ "$arg" = "-noexecute" ] 
+        then
+            noexec_flag=true;
+        fi
+    fi
+    i=$((i+1))
+done
+
 unzip_submissions "$1"
 
 organize_files "$1" "$2"
 
-execute_files "$2" "$3"
+if [ "$noexec_flag" = "false" ]
+then
+    execute_files "$2" "$3"
 
-csv_path="target/results.csv"
-echo "student_id,type,matched,not_matched" > "$csv_path"
+    csv_path="target/results.csv"
+    echo "student_id,type,matched,not_matched" > "$csv_path"
 
-match_files "$2" "$4" "$csv_path"
+    match_files "$2" "$4" "$csv_path"
+fi
+
